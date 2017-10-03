82ec097aded526dac7e68a9e590027d99dd5623c
Ivan Badichan <badican01117@gmail.com>
2017-10-03 22:57:06 +0200

    Use can sign in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "test#{n}example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password '123456789'
     password_confirmation '123456789'
     confirmed_at Time.now - 1.days
