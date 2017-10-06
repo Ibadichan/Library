378970e303aa743b7747addc0a6c3e367d9153ab
Ivan Badichan <badican01117@gmail.com>
2017-10-06 15:26:34 +0200

    Add name to user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,13 +23,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
+  OmniAuth.config.test_mode = true
 end
-
-OmniAuth.config.test_mode = true
