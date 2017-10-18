8ca183e6685cccc6711cdc92718a7131f145cf65
Ivan Badichan <badican01117@gmail.com>
2017-10-18 13:25:47 +0200

    Test external API, errors

--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Search do
   describe '.find_book' do
+    let(:query)  { 'abc' }
+    let(:object) { create(:user).to_json }
+
     it 'makes an api request to Google' do
-      stub_request(:get, 'https://www.googleapis.com/books/v1/volumes?q=Lenin')
+      stub_request(:get, "https://www.googleapis.com/books/v1/volumes?q=#{query}")
         .to_return(status: 200)
-      RestClient.get 'https://www.googleapis.com/books/v1/volumes?q=Lenin'
+      Search.find_book(query)
+    end
+
+    it 'returns json' do
+      stub_request(:get, "https://www.googleapis.com/books/v1/volumes?q=#{query}")
+        .to_return(status: 200, body: object)
+      expect(Search.find_book(query)).to be_a(Hash)
     end
   end
 end
