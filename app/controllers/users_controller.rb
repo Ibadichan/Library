d12c418952d2a582234ca12cb01844f2b67cbcfc
Ivan Badichan <badican01117@gmail.com>
2017-11-09 22:44:25 +0100

    User can search others users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
     respond_with @user.update(email: user_params[:email]) if request.patch? && user_params[:email]
   end
 
+  def search_others_users
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true).page params[:page]
+  end
+
   private
 
   def user_params
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe UsersController, type: :controller do
   describe 'PATCH #finish_sign_up' do
     it 'updates user email' do
       patch :finish_sign_up, params: { id: @user.id, user: { email: 'test@example.com' } }
-
-      @user.reload
       expect(assigns(:user).unconfirmed_email).to eq 'test@example.com'
     end
   end
@@ -23,6 +21,13 @@ RSpec.describe UsersController, type: :controller do
     it 'renders show template' do
       get :show, params: { id: @user.id }
       expect(response).to render_template 'show'
+    end
+  end
+
+  describe 'GET #search_others_users' do
+    it 'renders search_others_users.html.slim template' do
+      get :search_others_users, params: { id: @user.id }
+      expect(response).to render_template 'search_others_users'
     end
   end
 end
